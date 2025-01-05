cccbec57c325f268eddfe3fd961b222f7149125c
Ian Oswald <ian@ozzie.io>
2025-01-05 12:34:00 +1100

    home: init

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-24.11";
+    };
   };
 
   outputs = _: {
