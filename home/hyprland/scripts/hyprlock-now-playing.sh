378c1b5cb0e55f5539064a70f474b7698d39dff3
Ian Oswald <ian@ozzie.io>
2025-03-03 18:08:14 +1100

    hyprlock: config tuning

---
+++ home/hyprland/scripts/hyprlock-now-playing.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+status=$(playerctl status 2>/dev/null)
+if [[ -z $status ]]; then
+  exit
+fi
+
+if [[ $status == "Stopped" ]]; then
+  exit
+fi
+
+artist=$(playerctl metadata artist 2>/dev/null)
+title=$(playerctl metadata title 2>/dev/null)
+
+if [[ $status == "Playing" ]]; then
+  if [[ -z $artist ]]; then
+    echo 󰿎 $title
+    exit
+  fi
+
+  echo ♫ $artist - $title
+  exit
+fi
+
+if [[ -z $artist ]]; then
+  echo $title
+  exit
+fi
+
+echo $artist - $title
+exit
