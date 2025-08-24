44ea146e386ce6d80bbdd063c2f354684d3714ea
Ian Oswald <ian@ozzie.io>
2025-08-24 17:43:21 +1000

    hyprsunset: init

---
+++ home/hyprland/hyprsunset.nix
@@ -0,0 +1,72 @@
+{
+  pkgs,
+  ...
+}:
+{
+  home.file.".config/hypr/hyprsunset.conf".text = ''
+    max-gamma = 150
+
+    profile {
+      gamma = 0.9
+      temperature = 5500
+      time = 06:00
+    }
+
+    profile {
+      temperature = 6000
+      time = 06:30
+    }
+
+    profile {
+      temperature = 6500
+      time = 07:00
+    }
+
+    profile {
+      identity = true
+      time = 07:30
+    }
+
+    profile {
+      temperature = 6000
+      time = 18:00
+    }
+
+    profile {
+      temperature = 5500
+      time = 18:30
+    }
+
+    profile {
+      temperature = 5000
+      time = 19:00
+    }
+
+    profile {
+      temperature = 4500
+      time = 19:30
+    }
+
+    profile {
+      temperature = 4000
+      time = 20:00
+    }
+
+    profile {
+      gamma = 0.9
+      temperature = 4000
+      time = 20:30
+    }
+
+    profile {
+      gamma = 0.8
+      temperature = 4000
+      time = 21:00
+    }
+  '';
+
+  services.hyprsunset = {
+    enable = true;
+    package = with pkgs; hyprsunset;
+  };
+}
