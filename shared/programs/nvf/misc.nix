574a34c33a0cf3a9e52ead1323389cc3d8175498
Ian Oswald <ian@ozzie.io>
2025-09-20 16:38:35 +1000

    nvf: auto open diagnostic float

--- shared/programs/nvf/misc.nix
+++ shared/programs/nvf/misc.nix
@@ -15,6 +15,12 @@ in
 
       vim.api.nvim_create_autocmd('InsertEnter', { command = [[set norelativenumber]] })
       vim.api.nvim_create_autocmd('InsertLeave', { command = [[set relativenumber]] })
+
+      vim.api.nvim_create_autocmd({ "CursorHold", "InsertLeave" }, {
+        callback = function()
+          vim.diagnostic.open_float(nil, { focusable = false })
+        end,
+      })
     '';
   };
 }
