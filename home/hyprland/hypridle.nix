2d21b2f5560fde5dcd2545e90eaf1a7e73162935
Ian Oswald <ian@ozzie.io>
2025-10-01 02:39:52 +1000

    hypridle: make optional

--- home/hyprland/hypridle.nix
+++ home/hyprland/hypridle.nix
@@ -1,30 +1,33 @@
 {
-  pkgs,
+  config,
+  lib,
   ...
 }:
+let
+  cfg = config.ozzie.workstation.hypridle;
+in
 {
-  services.hypridle = {
-    enable = true;
-    package = with pkgs; hypridle;
+  config = lib.mkIf cfg.enable {
+    services.hypridle = {
+      settings = {
+        general = {
+          after_sleep_cmd = "hyprctl dispatch dpms on";
+          ignore_dbus_inhibit = false;
+          lock_cmd = "hyprlock";
+        };
 
-    settings = {
-      general = {
-        after_sleep_cmd = "hyprctl dispatch dpms on";
-        ignore_dbus_inhibit = false;
-        lock_cmd = "hyprlock";
+        listener = [
+          {
+            on-timeout = "hyprlock --grace 5";
+            timeout = 300;
+          }
+          {
+            on-resume = "hyprctl dispatch dpms on";
+            on-timeout = "hyprctl dispatch dpms off";
+            timeout = 360;
+          }
+        ];
       };
-
-      listener = [
-        {
-          on-timeout = "hyprlock";
-          timeout = 300;
-        }
-        {
-          on-resume = "hyprctl dispatch dpms on";
-          on-timeout = "hyprctl dispatch dpms off";
-          timeout = 600;
-        }
-      ];
     };
   };
 }
--- home/services/default.nix
+++ home/services/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
     ./flameshot.nix
+    ./hypridle.nix
   ];
 }
