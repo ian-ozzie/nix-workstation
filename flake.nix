9cab1bce69c6841f63aa6c976fed224b114582e1
Ian Oswald <ian@ozzie.io>
2026-03-06 16:02:00 +1100

    flake: update to unstable

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Ozzie's NixOS workstation configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master";
     };
 
     nvf = {
@@ -16,7 +16,7 @@
 
     stylix = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/stylix/release-25.11";
+      url = "github:nix-community/stylix/master";
     };
   };
 
